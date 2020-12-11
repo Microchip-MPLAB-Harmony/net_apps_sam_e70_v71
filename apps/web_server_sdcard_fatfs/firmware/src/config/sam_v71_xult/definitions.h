e963ea1ed8a7d28a360eecc69e40a5882a8b190f
Hrisikesh Sahu <hrisikesh.sahu@microchip.com>
2020-12-11 23:14:37 +0530

    [MH3-46386] [MH3-46387] [MH3-46388] [MH3-46389] web_server_sdcard_fatfs demo regeneration and test for V71 for both baremetal and freertos

--- apps/web_server_sdcard_fatfs/firmware/src/config/sam_v71_xult/definitions.h
+++ apps/web_server_sdcard_fatfs/firmware/src/config/sam_v71_xult/definitions.h
@@ -52,10 +52,6 @@
 #include "peripheral/efc/plib_efc.h"
 #include "peripheral/tc/plib_tc0.h"
 #include "system/time/sys_time.h"
-#include "net_pres/pres/net_pres.h"
-#include "net_pres/pres/net_pres_encryptionproviderapi.h"
-#include "net_pres/pres/net_pres_transportapi.h"
-#include "net_pres/pres/net_pres_socketapi.h"
 #include "peripheral/usart/plib_usart1.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
@@ -65,9 +61,7 @@
 #include "driver/gmac/drv_gmac.h"
 #include "driver/miim/drv_miim.h"
 #include "system/sys_time_h2_adapter.h"
-#include "system/sys_clk_h2_adapter.h"
 #include "system/sys_random_h2_adapter.h"
-#include "system/sys_reset_h2_adapter.h"
 #include "driver/sdmmc/drv_sdmmc.h"
 #include "system/command/sys_command.h"
 #include "peripheral/clk/plib_clk.h"
@@ -77,6 +71,10 @@
 #include "peripheral/xdmac/plib_xdmac.h"
 #include "bsp/bsp.h"
 #include "wolfssl/wolfcrypt/port/pic32/crypt_wolfcryptcb.h"
+#include "net_pres/pres/net_pres.h"
+#include "net_pres/pres/net_pres_encryptionproviderapi.h"
+#include "net_pres/pres/net_pres_transportapi.h"
+#include "net_pres/pres/net_pres_socketapi.h"
 #include "system/fs/sys_fs.h"
 #include "system/fs/sys_fs_media_manager.h"
 #include "system/fs/sys_fs_fat_interface.h"
@@ -209,8 +207,6 @@ Remarks:
 typedef struct
 {
     SYS_MODULE_OBJ  sysTime;
-    SYS_MODULE_OBJ  netPres;
-
     SYS_MODULE_OBJ  sysConsole0;
 
 
@@ -218,6 +214,8 @@ typedef struct
 
     SYS_MODULE_OBJ  drvMiim;
     SYS_MODULE_OBJ  sysDebug;
+
+    SYS_MODULE_OBJ  netPres;
 
     SYS_MODULE_OBJ  drvSDMMC0;
 
