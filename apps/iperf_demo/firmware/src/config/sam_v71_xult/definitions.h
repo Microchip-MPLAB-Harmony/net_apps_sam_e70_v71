c22c36e89efb33f058e89eb0f6780e2e1d44a4ca
Mohammed Fahad R <mohammedfahad.r@microchip.com>
2023-07-18 20:29:54 +0530

    [MH3-82591] iperf_demo application regeneration- SAMV71

--- apps/iperf_demo/firmware/src/config/sam_v71_xult/definitions.h
+++ apps/iperf_demo/firmware/src/config/sam_v71_xult/definitions.h
@@ -57,6 +57,7 @@
 #include "system/sys_time_h2_adapter.h"
 #include "system/sys_random_h2_adapter.h"
 #include "system/int/sys_int.h"
+#include "system/ports/sys_ports.h"
 #include "system/cache/sys_cache.h"
 #include "system/reset/sys_reset.h"
 #include "osal/osal.h"
@@ -83,6 +84,12 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* Device Information */
+#define DEVICE_NAME			 "ATSAMV71Q21B"
+#define DEVICE_ARCH			 "CORTEX-M7"
+#define DEVICE_FAMILY		 "SAMV"
+#define DEVICE_SERIES		 "SAMV71"
 
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 300000000
@@ -202,8 +209,8 @@ typedef struct
 
 
     SYS_MODULE_OBJ  tcpip;
+   SYS_MODULE_OBJ  drvMiim_0;
 
-    SYS_MODULE_OBJ  drvMiim;
     SYS_MODULE_OBJ  sysDebug;
 
 
