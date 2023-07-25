95a485e845e3096f629b89c0b2c32312f47d743b
Mohammed Fahad R <mohammedfahad.r@microchip.com>
2023-07-25 16:43:20 +0530

    [MH3-82592] iperf_demo application regeneration- SAMV71_freertos

--- apps/iperf_demo/firmware/src/config/sam_v71_xult_freertos/bsp/bsp.h
+++ apps/iperf_demo/firmware/src/config/sam_v71_xult_freertos/bsp/bsp.h
@@ -40,8 +40,8 @@
 *******************************************************************************/
 // DOM-IGNORE-END
 
-#ifndef _BSP_H
-#define _BSP_H
+#ifndef BSP_H
+#define BSP_H
 
 // *****************************************************************************
 // *****************************************************************************
@@ -60,9 +60,17 @@
 // Section: BSP Macros
 // *****************************************************************************
 // *****************************************************************************
-/*** VBUS Macros for VBUS_HOST_EN ***/
-#define VBUS_HOST_EN_PowerEnable() (PIOC_REGS->PIO_CODR = (1<<16))
-#define VBUS_HOST_EN_PowerDisable() (PIOC_REGS->PIO_SODR = (1<<16))
+#define sam_v71_xult
+#define BSP_NAME             "sam_v71_xult"
+
+/*** LED Macros for LED0 ***/
+#define LED0_Toggle() (PIOA_REGS->PIO_ODSR ^= (1UL<<23))
+#define LED0_On() (PIOA_REGS->PIO_CODR = (1UL<<23))
+#define LED0_Off() (PIOA_REGS->PIO_SODR = (1UL<<23))
+/*** SWITCH Macros for SWITCH0 ***/
+#define SWITCH0_Get() ((PIOA_REGS->PIO_PDSR >> 9) & 0x1)
+#define SWITCH0_STATE_PRESSED 0
+#define SWITCH0_STATE_RELEASED 1
 
 
 
@@ -96,7 +104,6 @@
 
   Example:
     <code>
-    //Initialize the BSP
     BSP_Initialize();
     </code>
 
@@ -106,7 +113,7 @@
 
 void BSP_Initialize(void);
 
-#endif // _BSP_H
+#endif // BSP_H
 
 /*******************************************************************************
  End of File
