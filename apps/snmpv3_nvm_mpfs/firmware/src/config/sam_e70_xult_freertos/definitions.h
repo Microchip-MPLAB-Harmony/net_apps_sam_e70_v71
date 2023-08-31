e48ae0b6bd04d0223d70d09ad82926efec16c32e
Mohammed Fahad R <mohammedfahad.r@microchip.com>
2023-08-31 14:45:30 +0530

    [MH3-83976] snmpv3_nvm_mpfs application regeneration- SAME70_freertos

--- apps/snmpv3_nvm_mpfs/firmware/src/config/sam_e70_xult_freertos/definitions.h
+++ apps/snmpv3_nvm_mpfs/firmware/src/config/sam_e70_xult_freertos/definitions.h
@@ -53,6 +53,7 @@
 #include "peripheral/efc/plib_efc.h"
 #include "peripheral/tc/plib_tc0.h"
 #include "system/time/sys_time.h"
+#include "bsp/bsp.h"
 #include "driver/memory/drv_memory_efc.h"
 #include "peripheral/usart/plib_usart1.h"
 #include "system/int/sys_int.h"
@@ -93,6 +94,12 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* Device Information */
+#define DEVICE_NAME			 "ATSAME70Q21B"
+#define DEVICE_ARCH			 "CORTEX-M7"
+#define DEVICE_FAMILY		 "SAME"
+#define DEVICE_SERIES		 "SAME70"
 
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 300000000
@@ -213,8 +220,8 @@ typedef struct
 
 
     SYS_MODULE_OBJ  tcpip;
+   SYS_MODULE_OBJ  drvMiim_0;
 
-    SYS_MODULE_OBJ  drvMiim;
     SYS_MODULE_OBJ  sysDebug;
 
     SYS_MODULE_OBJ  netPres;
