098831599c5a7f33089b2aba7f6d5fc95ef80005
Mohammed Fahad R <mohammedfahad.r@microchip.com>
2023-08-31 15:48:16 +0530

    [MH3-84046] web_ftp_server_mpfs_fatfs application regeneration - SAME70_freertos

--- apps/web_ftp_server_mpfs_fatfs/firmware/src/config/sam_e70_xult_freertos/definitions.h
+++ apps/web_ftp_server_mpfs_fatfs/firmware/src/config/sam_e70_xult_freertos/definitions.h
@@ -51,14 +51,29 @@
 #include "crypto/crypto.h"
 #include "driver/memory/drv_memory.h"
 #include "peripheral/efc/plib_efc.h"
+#include "peripheral/tc/plib_tc0.h"
 #include "system/time/sys_time.h"
 #include "bsp/bsp.h"
+#include "driver/memory/drv_memory_efc.h"
+#include "peripheral/usart/plib_usart1.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "system/reset/sys_reset.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
+#include "library/tcpip/tcpip.h"
+#include "system/sys_time_h2_adapter.h"
+#include "system/sys_random_h2_adapter.h"
+#include "driver/gmac/drv_gmac.h"
+#include "system/command/sys_command.h"
+#include "driver/sdmmc/drv_sdmmc.h"
+#include "peripheral/clk/plib_clk.h"
+#include "peripheral/pio/plib_pio.h"
+#include "peripheral/nvic/plib_nvic.h"
+#include "peripheral/mpu/plib_mpu.h"
+#include "peripheral/xdmac/plib_xdmac.h"
 #include "driver/miim/drv_miim.h"
+#include "wolfssl/wolfcrypt/port/pic32/crypt_wolfcryptcb.h"
 #include "net_pres/pres/net_pres.h"
 #include "net_pres/pres/net_pres_encryptionproviderapi.h"
 #include "net_pres/pres/net_pres_transportapi.h"
@@ -70,26 +85,11 @@
 #include "system/fs/fat_fs/file_system/ffconf.h"
 #include "system/fs/fat_fs/hardware_access/diskio.h"
 #include "system/fs/mpfs/mpfs.h"
-#include "peripheral/hsmci/plib_hsmci.h"
-#include "peripheral/tc/plib_tc0.h"
-#include "driver/memory/drv_memory_efc.h"
-#include "peripheral/usart/plib_usart1.h"
-#include "library/tcpip/tcpip.h"
-#include "system/sys_time_h2_adapter.h"
-#include "system/sys_random_h2_adapter.h"
-#include "driver/gmac/drv_gmac.h"
-#include "driver/sdmmc/drv_sdmmc.h"
-#include "system/command/sys_command.h"
-#include "peripheral/clk/plib_clk.h"
-#include "peripheral/pio/plib_pio.h"
-#include "peripheral/nvic/plib_nvic.h"
-#include "peripheral/mpu/plib_mpu.h"
-#include "peripheral/xdmac/plib_xdmac.h"
-#include "wolfssl/wolfcrypt/port/pic32/crypt_wolfcryptcb.h"
 #include "system/console/sys_console.h"
 #include "system/console/src/sys_console_uart_definitions.h"
 #include "FreeRTOS.h"
 #include "task.h"
+#include "peripheral/hsmci/plib_hsmci.h"
 #include "app.h"
 
 
@@ -101,6 +101,12 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* Device Information */
+#define DEVICE_NAME			 "ATSAME70Q21B"
+#define DEVICE_ARCH			 "CORTEX-M7"
+#define DEVICE_FAMILY		 "SAME"
+#define DEVICE_SERIES		 "SAME70"
 
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 300000000
@@ -216,16 +222,16 @@ Remarks:
 typedef struct
 {
     SYS_MODULE_OBJ  sysTime;
+    SYS_MODULE_OBJ  drvMemory0;
     SYS_MODULE_OBJ  sysConsole0;
 
 
-    SYS_MODULE_OBJ  drvMiim;
-    SYS_MODULE_OBJ  netPres;
-
-    SYS_MODULE_OBJ  drvMemory0;
-
     SYS_MODULE_OBJ  tcpip;
+   SYS_MODULE_OBJ  drvMiim_0;
+
     SYS_MODULE_OBJ  sysDebug;
+
+    SYS_MODULE_OBJ  netPres;
 
     SYS_MODULE_OBJ  drvSDMMC0;
 
