975c494a0aadf8c9d33898772f0175a3dac7d4bc
Mohammed Fahad R <mohammedfahad.r@microchip.com>
2023-09-20 09:28:44 +0530

    [MH3-84096] tcpip_udp_server application regeneration - SAME70

--- apps/tcpip_udp_server/firmware/src/config/sam_e70_xult/library/tcpip/tcpip.h
+++ apps/tcpip_udp_server/firmware/src/config/sam_e70_xult/library/tcpip/tcpip.h
@@ -731,6 +731,7 @@ typedef struct TCPIP_STACK_INIT
 #include "tcpip/tcp.h"
 #include "tcpip/udp.h"
 #include "tcpip/icmp.h"
+#include "tcpip/tcpip_announce.h"
 #include "tcpip/lldp.h"
 #include "tcpip/tcpip_commands.h"
 #endif  // __TCPIP_H__
