8115be9bdab759ebe6ee1c80ef9733eacf3f8774
hrisikesh sahu <hrisikesh.sahu@microchip.com>
2023-10-05 22:15:20 +0530

    [MH3-84048] web_net_server_nvm_mpfs application regeneration - SAME70

--- apps/web_net_server_nvm_mpfs/firmware/src/config/sam_e70_xult/configuration.h
+++ apps/web_net_server_nvm_mpfs/firmware/src/config/sam_e70_xult/configuration.h
@@ -207,12 +207,6 @@ extern "C" {
 #define TCPIP_ICMP_COMMAND_ENABLE              false
 
 
-/*** NBNS Configuration ***/
-#define TCPIP_STACK_USE_NBNS
-#define TCPIP_NBNS_TASK_TICK_RATE   110
-
-
-
 /*** HTTP NET Configuration ***/
 #define TCPIP_STACK_USE_HTTP_NET_SERVER
 #define TCPIP_HTTP_NET_MAX_HEADER_LEN		    		15
@@ -259,6 +253,12 @@ extern "C" {
 #define TCPIP_HTTP_NET_MALLOC_FUNC                  malloc
 #define TCPIP_HTTP_NET_FREE_FUNC                    free
 #define TCPIP_HTTP_NET_CONSOLE_CMD           		false
+
+
+
+/*** NBNS Configuration ***/
+#define TCPIP_STACK_USE_NBNS
+#define TCPIP_NBNS_TASK_TICK_RATE   110
 
 
 /******************************************************************************/
--- apps/web_net_server_nvm_mpfs/firmware/src/config/sam_e70_xult/initialization.c
+++ apps/web_net_server_nvm_mpfs/firmware/src/config/sam_e70_xult/initialization.c
@@ -339,7 +339,8 @@ const TCPIP_STACK_MODULE_CONFIG TCPIP_STACK_MODULE_CONFIG_TBL [] =
     {TCPIP_MODULE_SNTP,             &tcpipSNTPInitData},            // TCPIP_MODULE_SNTP
 
     {TCPIP_MODULE_HTTP_NET_SERVER,  &tcpipHTTPNetInitData},         // TCPIP_MODULE_HTTP_NET_SERVER
-    {TCPIP_MODULE_SMTPC, &tcpipSMTPCInitData},                                  // TCPIP_MODULE_SMTPC,
+    {TCPIP_MODULE_SMTPC,            &tcpipSMTPCInitData},           // TCPIP_MODULE_SMTPC,
+    {TCPIP_MODULE_COMMAND,          0},                             // TCPIP_MODULE_COMMAND,
     { TCPIP_MODULE_MANAGER,         &tcpipHeapConfig },             // TCPIP_MODULE_MANAGER
 
 // MAC modules
