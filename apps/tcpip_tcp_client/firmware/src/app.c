eaaa882435b021836e95ddd9828dc7cb6225fe77
Hrisikesh Sahu <hrisikesh.sahu@microchip.com>
2023-11-08 20:13:04 +0530

    [MH3-87032] Optimization O2 build issue resolved

--- apps/tcpip_tcp_client/firmware/src/app.c
+++ apps/tcpip_tcp_client/firmware/src/app.c
@@ -96,7 +96,7 @@ APP_DATA appData;
 int32_t _APP_ParseUrl(char *uri, char **host, char **path, uint16_t * port);
 int8_t _APP_PumpDNS(const char * hostname, IPV4_ADDR *ipv4Addr);
 
-
+bool gDataReceived=false;
 
 // *****************************************************************************
 // *****************************************************************************
@@ -306,7 +306,7 @@ void APP_Tasks ( void )
                     "Host: %s\r\n"
                     "Connection: close\r\n\r\n", appData.path ? appData.path : "null" , appData.host);
             TCPIP_TCP_ArrayPut(appData.socket, (uint8_t*)buffer, strlen(buffer));
-            appData.state = APP_TCPIP_WAIT_FOR_RESPONSE;
+            appData.state = APP_TCPIP_WAIT_FOR_RESPONSE;            
         }
         break;
 
@@ -318,6 +318,12 @@ void APP_Tasks ( void )
             {
                 TCPIP_TCP_ArrayGet(appData.socket, (uint8_t*)buffer, sizeof(buffer) - 1);
                 SYS_CONSOLE_PRINT("%s", buffer);
+                gDataReceived = true;
+            }
+            if(gDataReceived)
+            {
+                SYS_CONSOLE_PRINT("\r\nConnection established with < %s > \r\n",appData.host);
+                gDataReceived = false;
             }
 
             if (!TCPIP_TCP_IsConnected(appData.socket) || TCPIP_TCP_WasDisconnected(appData.socket))
